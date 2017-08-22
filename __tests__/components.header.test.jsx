619c88cee81d2c85cef445e2a7c70b84c6fb739f
Keisuke Kan <9renpoto@gmail.com>
2017-08-22 16:11:12 +0900

    feat(__tests__) use jest

--- __tests__/components.header.test.jsx
+++ __tests__/components.header.test.jsx
@@ -1,14 +1,12 @@
 /* @flow */
-import { describe, it } from 'mocha'
 import { shallow } from 'enzyme'
 import { internet, random } from 'faker'
-import assert from 'assert'
 
 import React from 'react'
 import { Header } from '../src/components/Header'
 
 describe('header', () => {
-  it('render', () => {
+  test('render', () => {
     const wrapper = shallow(
       <Header
         baseUrl={internet.url()}
@@ -19,6 +17,6 @@ describe('header', () => {
         titles={[random.word(), random.word()]}
       />
     )
-    assert(wrapper.length === 1)
+    expect(wrapper.length).toBe(1)
   })
 })
